7f3d482f8fcaa8d01b7afdb58f2d0f91eef1977f
EdgarEldy <ed.eldy21@live.com>
2026-08-01 15:23:06 +0400

    feat(core-architecture): add components/AppFooter.vue

---
+++ frontend/src/components/AppFooter.vue
@@ -0,0 +1,7 @@
+<script setup>
+const year = new Date().getFullYear()
+</script>
+
+<template>
+  <v-footer app class="justify-center text-caption">&copy; {{ year }} MEVN Tutorial</v-footer>
+</template>
